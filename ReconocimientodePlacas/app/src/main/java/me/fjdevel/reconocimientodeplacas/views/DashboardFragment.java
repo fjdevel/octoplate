89936328d085b4344a61d0d58fa0c96622dcc1fb
Frederick Mejia <fjdevel@gmail.com>
2018-06-27 00:01:43 -0600

    ya solucione la compatibilidad de los fragments

---
+++ ReconocimientodePlacas/app/src/main/java/me/fjdevel/reconocimientodeplacas/views/DashboardFragment.java
@@ -0,0 +1,30 @@
+package me.fjdevel.reconocimientodeplacas.views;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.fjdevel.reconocimientodeplacas.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DashboardFragment extends Fragment {
+
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+}
